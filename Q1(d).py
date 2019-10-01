f8881f89cb04bd55df6d2c2e264ccf1ddc9e93bb
abhisheka950 <54525730+abhisheka950@users.noreply.github.com>
2019-10-01 20:17:12 +0530

    hbhjjb

---
+++ Q1(d).py
@@ -0,0 +1,16 @@
+N=5
+for i in range(1,6):
+    if i%2==1:
+        for j in range(1,18):
+            print('0',end="")
+        print()
+    elif i==4:
+        for j in range(1,5):
+            print('0',end='')
+        print()
+    else:
+        for j in range(1,8):
+            print('0',end="")
+        print()
+            
+    
